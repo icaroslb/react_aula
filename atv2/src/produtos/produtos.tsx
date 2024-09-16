3136f4c81ba981f6a96b6f654b3afc1a4d9be793
Ícaro Barbosa <31295743+icaroslb@users.noreply.github.com>
2024-09-16 00:16:01 -0300

    Atividade 2

---
+++ atv2/src/produtos/produtos.tsx
@@ -0,0 +1,49 @@
+const produtos = [
+  {
+    id: 1,
+    nome: 'Smartphone',
+    preco: 'R$ 2000',
+    cores: ['#29d8d5', '#252a34', '#fc3766'],
+  },
+  {
+    id: 2,
+    nome: 'Notebook',
+    preco: 'R$ 3000',
+    cores: ['#ffd045', '#d4394b', '#f37c59'],
+  },
+  {
+    id: 3,
+    nome: 'Tablet',
+    preco: 'R$ 1500',
+    cores: ['#365069', '#47c1c8', '#f95786'],
+  },
+];
+
+const Produtos = () => {
+  const produtos_filtrados = produtos.filter(({ preco }) => {
+    return Number(preco.replace('R$ ', '')) > 1500;
+  });
+
+  return (
+    <>
+      {produtos_filtrados.map(({ id, nome, preco, cores }) => (
+        <div key={id + nome}>
+          <h1>{nome}</h1>
+          <p>Preço: {preco}</p>
+          <ul>
+            {cores.map((cor) => (
+              <li
+                key={id + cor}
+                style={{ backgroundColor: cor, color: 'white' }}
+              >
+                {cor}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Produtos;
